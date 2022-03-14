97c0401a214c80fa9575c49c3a37a7b506263088
Ceyda Keklik <ceydamehves@gmail.com>
2022-03-14 20:39:30 +0300

    Search & Clear

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = (props) => {
+
+    const [currentValue, setCurrentValue] = useState('')
+    
 	return (
 		<div>
-            <input 
-            className = 'form-control' 
-            placeholder = 'search'
-            value = {props.value} 
-            onChange = {(event) => props.setSearchMovies(event.target.value)}></input>
+                <input 
+                    className = 'form-control' 
+                    placeholder = 'search'            
+                    value = {props.value}
+                    onChange={(event) => setCurrentValue(event.target.value)}/>
+                    <div>
+                    <button onClick={(event) => props.setSearchMovies('')}>Clear</button>
+                    <button onClick={(event) => props.setSearchMovies(currentValue)}>Search</button>
+                    </div>
+            
         </div>
 	);
 };
